a0659d41ff6f935549b4ca50da74e00513aa3fd8
USUARIO <je.gacituab@gmail.com>
2018-04-27 17:29:33 -0300

    no message

---
+++ example/src/controllers/controllerinicio.js
@@ -0,0 +1,33 @@
+'use strict';
+
+app.controller('controllerInicio', function($scope,$http, $state, $location){
+	
+
+
+
+$scope.cerrasesion = function(){
+		$state.go('login');
+}
+	
+
+
+
+
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
